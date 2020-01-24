25db0b6a1f5a510ac12ef0953c82edc33d4989a7
gksidburycrawford <45250978+gksidburycrawford@users.noreply.github.com>
2020-01-24 10:37:00 -0500

    Add files via upload

---
+++ City Bike Rentals/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBikes
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picBikes = New System.Windows.Forms.PictureBox()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblCostHeading = New System.Windows.Forms.Label()
+        Me.lblNumberOfBikes = New System.Windows.Forms.Label()
+        Me.txtNumberOfBikes = New System.Windows.Forms.TextBox()
+        Me.lblTotalCostLabel = New System.Windows.Forms.Label()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        Me.btnCalculateCost = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.picBikes, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picBikes
+        '
+        Me.picBikes.Image = Global.City_Bike_Rentals.My.Resources.Resources.Bikes
+        Me.picBikes.Location = New System.Drawing.Point(1, 285)
+        Me.picBikes.Name = "picBikes"
+        Me.picBikes.Size = New System.Drawing.Size(407, 175)
+        Me.picBikes.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBikes.TabIndex = 0
+        Me.picBikes.TabStop = False
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Cooper Black", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.lblHeading.Location = New System.Drawing.Point(91, 9)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(226, 27)
+        Me.lblHeading.TabIndex = 1
+        Me.lblHeading.Text = "City Bike Rentals"
+        '
+        'lblCostHeading
+        '
+        Me.lblCostHeading.AutoSize = True
+        Me.lblCostHeading.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCostHeading.Location = New System.Drawing.Point(101, 36)
+        Me.lblCostHeading.Name = "lblCostHeading"
+        Me.lblCostHeading.Size = New System.Drawing.Size(206, 20)
+        Me.lblCostHeading.TabIndex = 2
+        Me.lblCostHeading.Text = "$X.XX per Bike for 24 hours"
+        '
+        'lblNumberOfBikes
+        '
+        Me.lblNumberOfBikes.AutoSize = True
+        Me.lblNumberOfBikes.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumberOfBikes.Location = New System.Drawing.Point(63, 79)
+        Me.lblNumberOfBikes.Name = "lblNumberOfBikes"
+        Me.lblNumberOfBikes.Size = New System.Drawing.Size(180, 20)
+        Me.lblNumberOfBikes.TabIndex = 3
+        Me.lblNumberOfBikes.Text = "Number of Bike Rentals:"
+        '
+        'txtNumberOfBikes
+        '
+        Me.txtNumberOfBikes.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumberOfBikes.Location = New System.Drawing.Point(293, 76)
+        Me.txtNumberOfBikes.Name = "txtNumberOfBikes"
+        Me.txtNumberOfBikes.Size = New System.Drawing.Size(50, 26)
+        Me.txtNumberOfBikes.TabIndex = 4
+        Me.txtNumberOfBikes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblTotalCostLabel
+        '
+        Me.lblTotalCostLabel.AutoSize = True
+        Me.lblTotalCostLabel.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCostLabel.Location = New System.Drawing.Point(63, 115)
+        Me.lblTotalCostLabel.Name = "lblTotalCostLabel"
+        Me.lblTotalCostLabel.Size = New System.Drawing.Size(199, 20)
+        Me.lblTotalCostLabel.TabIndex = 5
+        Me.lblTotalCostLabel.Text = "Total Cost of Bikes Rented:"
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.AutoSize = True
+        Me.lblTotalCost.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCost.Location = New System.Drawing.Point(289, 115)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(61, 20)
+        Me.lblTotalCost.TabIndex = 6
+        Me.lblTotalCost.Text = "$888.88"
+        '
+        'btnCalculateCost
+        '
+        Me.btnCalculateCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculateCost.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.btnCalculateCost.Location = New System.Drawing.Point(67, 211)
+        Me.btnCalculateCost.Name = "btnCalculateCost"
+        Me.btnCalculateCost.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculateCost.TabIndex = 7
+        Me.btnCalculateCost.Text = "Calculate Cost"
+        Me.btnCalculateCost.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.btnClear.Location = New System.Drawing.Point(167, 211)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.btnExit.Location = New System.Drawing.Point(268, 211)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmBikes
+        '
+        Me.AcceptButton = Me.btnCalculateCost
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(409, 461)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculateCost)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.lblTotalCostLabel)
+        Me.Controls.Add(Me.txtNumberOfBikes)
+        Me.Controls.Add(Me.lblNumberOfBikes)
+        Me.Controls.Add(Me.lblCostHeading)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.picBikes)
+        Me.Name = "frmBikes"
+        Me.Text = "City Bike Rentals"
+        CType(Me.picBikes, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents picBikes As PictureBox
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblCostHeading As Label
+    Friend WithEvents lblNumberOfBikes As Label
+    Friend WithEvents txtNumberOfBikes As TextBox
+    Friend WithEvents lblTotalCostLabel As Label
+    Friend WithEvents lblTotalCost As Label
+    Friend WithEvents btnCalculateCost As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ City Bike Rentals/Form1.vb
@@ -0,0 +1,80 @@
+﻿' Program:  City Bike Rental
+' Author:   Geterry Sidbury-Crawford
+' Date:     Oct 11, 2017
+' Purpose:  This application calculates and displays the total cost
+'           of renting multiple bikes for a 24 hour period.
+
+Option Strict On
+
+
+Public Class frmBikes
+
+    ' Class level constant used in multiple modules
+    Const _cdecPricePerBike As Decimal = 9.95D
+
+    Private Sub btnCalculateCost_Click(sender As Object, e As EventArgs) Handles btnCalculateCost.Click
+        ' This event handler calculates and displays the cost of the bikes
+        ' rented (number of bikes times the cost per bike).
+
+        'Dim strNumberofBikes As String
+        'Dim intNumberofBikes As Integer
+        'Dim DecTotalCost As Decimal
+
+
+
+        'strNumberofBikes = txtNumberOfBikes.Text
+        'intNumberofBikes = Convert.ToInt32(strNumberofBikes)
+        'DecTotalCost = intNumberofBikes * _cdecPricePerBike
+
+        'lblTotalCost.Text = DecTotalCost.ToString("C")
+
+        'Input variables
+        Dim strHeightInInches As String
+        Dim intHeightInInches As Integer
+
+        Dim intHeightInFeet As Integer
+        Dim intRemainderInches As Integer
+
+        'Output varibles
+        strHeightInInches = txtNumberOfBikes.Text       'Memorize this!!
+        intHeightInInches = Convert.ToInt32(strHeightInInches) 'MEMORIZE THIS TOO!!!
+
+        intHeightInFeet = intHeightInInches \ 12
+        intRemainderInches = intHeightInInches Mod 12
+
+        lblTotalCost.Text = "you are " & intHeightInFeet.ToString() & " feet and " _
+            & intRemainderInches.ToString() & " inches "
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'this event handler clear the number of bikes and clears the
+        'total cost
+
+        lblTotalCost.Text = ""
+        txtNumberOfBikes.Clear()
+        txtNumberOfBikes.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' This event handler closes the form and exits the program.
+
+        Close()
+
+    End Sub
+
+    Private Sub frmBikes_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' this event handler sets the price per bike in the cost heading,
+        ' clears the total cost label, and sets the focus to the number of
+        ' bikes textbox.
+
+        lblTotalCost.Text = ""
+        lblCostHeading.Text = _cdecPricePerBike.ToString("C") & " per Bike for 24 hours"
+
+
+        txtNumberOfBikes.Focus()
+
+    End Sub
+End Class
